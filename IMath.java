cc3d46e4e3e6584281bc751a5a6c3eead6881b65
Tulikasharma <tulikasharma368@gmail.com>
2021-08-21 13:14:58 +0530

    Refractored to paas lambda function to show result

--- IMath.java
+++ IMath.java
@@ -2,5 +2,9 @@ package lambdaAndStream;
 
 @FunctionalInterface
 public interface IMath {
-	int calculate(int a,int b);
+	int calculate(int a,int b);// only one abstract method
+	
+	public static void print(int a, int b, String function,IMath fobj ) {
+		System.out.println("Result of "+function+" is "+fobj.calculate(a,b));
+	}
 }
--- LambdaExpressions.java
+++ LambdaExpressions.java
@@ -5,11 +5,17 @@ public class LambdaExpressions {
 	public static void main(String[] args) {
 		
 		IMath add = (int a,int b) -> a+b ;// add is a lambda function here
-		IMath mul = (int a,int b) -> a*b ;
+		IMath mul = (int a,int b) -> a*b ;//these are anonymous functions, they don't belong to any class
 		IMath div = (int a,int b) -> a/b ;
 		
-		System.out.println("Sum of 2 numbers: "+add.calculate(10, 20));
-		System.out.println("Product of 2 numbers: "+mul.calculate(10, 20));
-		System.out.println("Division of 2 numbers: "+div.calculate(100, 20));
+//		System.out.println("Sum of 2 numbers: "+add.calculate(10, 20));
+//		System.out.println("Product of 2 numbers: "+mul.calculate(10, 20));
+//		System.out.println("Division of 2 numbers: "+div.calculate(100, 20));
+		
+		IMath.print(200, 10, "Addition", add);
+		IMath.print(200, 10, "Multiplication", mul);
+		IMath.print(200, 10, "Division", div);
+		
+		
 	}
 }
